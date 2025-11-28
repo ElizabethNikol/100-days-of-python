b73108469e17c62e14398fb2f35573afc7076ec7
Elizabeth Nikolaev <elizabethnikolaev@gmail.com>
2025-11-28 20:52:00 +0200

    Create hackerrank_solutions.py

---
+++ practice/hackerrank_solutions.py
@@ -0,0 +1,21 @@
+# Problem 1: Python If-Else
+def if_else_solution(n):
+    if n % 2 == 1:
+        print("Weird")
+    elif n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+# Problem 2: Arithmetic Operators
+def arithmetic_operators(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+# Problem 3: Division
+def division(a, b):
+    print(a // b)
+    print(a / b)
